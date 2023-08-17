43c2c50c362f13dc8aba22b78b4bc49c0e5f4ef2
Austin Oblow <austinoblow97@gmail.com>
2023-08-17 18:34:32 -0500

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadImages();
+    loadBreads();
+});
+
+function loadImages() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((resp) => resp.json())
+    .then((data) => {
+        data.message.forEach(image => addImage(image));
+    });
+}
+
+function addImage(picUrl) {
+    const container = document.getElementById("dog-image-container");
+    const newImage = document.createElement("Img");
+    newImage.src = picUrl;
+    container.appendChild(newImage);
+}
+
+function loadBreads() {
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then ((resp) => resp.json())
+    .then ((data) => {
+        const breeds = Object.keys(data.message);
+        addBreeds(breeds);
+    });
+}
+
+function addBreeds(breeds) {
+    const ul = document.getElementById("dog-breeds");
+    breeds.forEach(breed => {
+        const li = document.createElement("li");
+        li.innerText = breed;
+        ul.appendChild(li);
+        li.addEventListener('click', function(e) {
+            e.target.style.color = "blue";
+        });
+    });
+}
